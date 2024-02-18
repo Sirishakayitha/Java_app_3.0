39ecddb1f2e70631d074ed11340780ca453371a6
Sirishakayitha <119124831+Sirishakayitha@users.noreply.github.com>
2024-02-18 17:47:09 +0000

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -5,10 +5,10 @@ import subprocess
 
 def jfrogUpload() :
     # define the url file path, and authentication credentials and change ur IP Address
-    url = 'http://18.132.3.185:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.2-SNAPSHOT.jar'
+    url = 'http://ipaddress/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.2-SNAPSHOT.jar'
     file_path = '/var/lib/jenkins/workspace/project/target/kubernetes-configmap-reload-0.0.2-SNAPSHOT.jar'
     username = 'admin'
-    password = 'Password1*' #replace 'your password with ur actual password'
+    password = 'password' #replace 'your password with ur actual password'
 
     #send the PUT request with authentication and file upload
     with open(file_path, 'rb') as file:
@@ -22,7 +22,7 @@ def jfrogUpload() :
         print(response.text)
 def mvnBuild() :
     #define the Maven Command
-    maven_command = "mvn clena install -DskipTests"
+    maven_command = "mvn clean install -DskipTests"
 
     #Run the maven command as a subprocess
     try:
